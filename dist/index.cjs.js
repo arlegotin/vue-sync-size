2f181026ce4d7b3b338e0139a31e3f3e38a53d48
Artem Legotin <arlegotin@gmail.com>
2022-10-27 18:16:48 +0200

    Update docs

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -119,8 +119,7 @@ class Observers {
   }
 }
 
-function index () {
-  const observers = new Observers();
+function vue3(observers) {
   return {
     mounted: (observingElement, {
       value: referenceElement,
@@ -141,6 +140,40 @@ function index () {
       observers.remove(referenceElement, observingElement, side);
     }
   };
+}
+function vue2(observers) {
+  return {
+    inserted: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.add(referenceElement, observingElement, side);
+    },
+    update: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.add(referenceElement, observingElement, side);
+    },
+    unbind: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.remove(referenceElement, observingElement, side);
+    }
+  };
+}
+function index ({
+  version = 3
+} = {}) {
+  const observers = new Observers();
+  if (version === 3) {
+    return vue3(observers);
+  }
+  if (version === 2) {
+    return vue2(observers);
+  }
+  throw new Error(`Unknown version "${version}"`);
 }
 
 module.exports = index;
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -113,8 +113,7 @@ class Observers {
   }
 }
 
-function index () {
-  const observers = new Observers();
+function vue3(observers) {
   return {
     mounted: (observingElement, {
       value: referenceElement,
@@ -135,6 +134,40 @@ function index () {
       observers.remove(referenceElement, observingElement, side);
     }
   };
+}
+function vue2(observers) {
+  return {
+    inserted: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.add(referenceElement, observingElement, side);
+    },
+    update: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.add(referenceElement, observingElement, side);
+    },
+    unbind: (observingElement, {
+      value: referenceElement,
+      arg: side = Observers.SIDE_BOTH
+    }) => {
+      observers.remove(referenceElement, observingElement, side);
+    }
+  };
+}
+function index ({
+  version = 3
+} = {}) {
+  const observers = new Observers();
+  if (version === 3) {
+    return vue3(observers);
+  }
+  if (version === 2) {
+    return vue2(observers);
+  }
+  throw new Error(`Unknown version "${version}"`);
 }
 
 export { index as default };
